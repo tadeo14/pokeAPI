a4a349a15200e5982737771df1f0613c0b5cc371
tadeo <tadeoacosta14@gmail.com>
2024-06-23 18:20:45 -0300

    creamos la funcion para que nos traiga un pokemon alaeatoriamente

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,22 @@ function App() {
 
       try {
         const respuesta = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
-        setListaPokemones(respuesta.data.result); //aqui esta ubicada la lista de pokemones 
+        setListaPokemones(respuesta.data.results); //aqui esta ubicada la lista de pokemones 
+        pokemomAleatorio(respuesta.data.results);
       } catch (error) {
         console.log(error);
       }
     };
     obtenerPokemones();
   }, [])
-  
+  const pokemomAleatorio = (listaPokemon) => {
+    //vamos a generar un numero aleatorio
+    const indiceAleatorio = Math.floor(Math.random() * 100);
+
+    const pokemon = listaPokemon[indiceAleatorio];
+    console.log(pokemon.name);
+
+    };
 
   return (
     <>
