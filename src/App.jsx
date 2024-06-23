a3cc282624093856af876129c73bc6e4e5b7e3b2
tadeo <tadeoacosta14@gmail.com>
2024-06-23 19:06:48 -0300

    generacion de las opciones que tiene el usuario para seleccionar el pokemon correcto

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 function App() {
   
   const [listaPokemones, setListaPokemones] = useState([]); 
+  const [pokemonActual, setPokemonActual] = useState('');
   
   useEffect(() => {    
     const obtenerPokemones = async () => {
@@ -19,15 +20,44 @@ function App() {
     };
     obtenerPokemones();
   }, [])
+
+  //GENEERAMOS EL POKEMON ALEATORIO 
   const pokemomAleatorio = (listaPokemon) => {
     //vamos a generar un numero aleatorio
     const indiceAleatorio = Math.floor(Math.random() * 100);
+    //usamos el array de lista pokemon y le ponemos como indice el numero aleatorio, trayendo un pokemon
+    //aleatorio
+    const pokemonAleatorio = listaPokemon[indiceAleatorio];
+    //guardamos el pokemon aleatorio en un estado 
+    setPokemonActual(pokemonAleatorio);
 
-    const pokemon = listaPokemon[indiceAleatorio];
-    console.log(pokemon.name);
-
+    generarOpciones(pokemonAleatorio,listaPokemon);
     };
 
+  //generar respuesta aleatorias 
+  const generarOpciones = (pokemonElegido, listaPokemon) => {
+    //creamos un array donde almacenamos los nombres de los pokemones que vamos a mostrar en las opciones
+    //a elegir
+    const opcionesGneradas = [pokemonElegido.name];
+  
+    while (opcionesGneradas.length < 4) {
+      //generamos un numero aleatorio para elegir un pokemon de la lista
+      const pokemonAleatorio = listaPokemon[Math.floor(Math.random() * 100)].name;
+      
+      //verificamos que el pokemon aleatorio no este ya en la lista de opciones
+      if (!opcionesGneradas.includes(pokemomAleatorio)) {
+        opcionesGneradas.push(pokemomAleatorio);
+      }
+
+     
+    }
+    console.log(opcionesGneradas);
+  
+  };
+  
+  
+  
+  
   return (
     <>
       
