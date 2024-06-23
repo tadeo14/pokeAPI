e4cde63067be3f3309a3c77e2df2eee9132bce71
tadeo <tadeoacosta14@gmail.com>
2024-06-23 18:03:45 -0300

    guardamos lo que nos trae la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './css/style.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  useEffect(() => {
-    
+  
+  const [listaPokemones, setListaPokemones] = useState([]); 
+  
+  useEffect(() => {    
     const obtenerPokemones = async () => {
 
       try {
-        const respuesta = axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
-        console.log(respuesta);
+        const respuesta = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=100');
+        setListaPokemones(respuesta.data.result); //aqui esta ubicada la lista de pokemones 
       } catch (error) {
         console.log(error);
       }
